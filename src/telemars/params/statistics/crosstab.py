d51f17e2b14e2dcdb2829d88f9e49d432391596c
Aleksandr P. <prosvirnin.a@outlook.com>
2025-10-24 16:01:41 +0300

    refactor: добавить комментарии к статистикам
    Для каждой статистики в перечислении указано,
    зависит ли она от целевой аудитории.

--- src/telemars/params/statistics/crosstab.py
+++ src/telemars/params/statistics/crosstab.py
@@ -11,13 +11,13 @@ class K7Statistic(Enum):
     CUM_REACH_PER = 'CumReachPer'  # Зависит от ЦА.
     SPOT_BY_BREAKS_CUM_REACH_PER = 'SpotByBreaksCumReachPer'  # Зависит от ЦА.
     RTG000_SUM = 'Rtg000Sum'  # Зависит от ЦА.
-    SALES_RTG000_SUM = 'SalesRtg000Sum'
+    SALES_RTG000_SUM = 'SalesRtg000Sum'  # Не зависит от ЦА.
     SPOT_BY_BREAKS_RTG000_SUM = 'SpotByBreaksRtg000Sum'  # Зависит от ЦА.
     SPOT_BY_BREAKS_SALES_RTG000_SUM = 'SpotByBreaksSalesRtg000Sum'  # Не зависит от ЦА.
     RTG_PER_SUM = 'RtgPerSum'  # Зависит от ЦА.
-    STAND_RTG_PER_SUM = 'StandRtgPerSum'
-    SALES_RTG_PER_SUM = 'SalesRtgPerSum'
-    STAND_SALES_RTG_PER_SUM = 'StandSalesRtgPerSum'
+    STAND_RTG_PER_SUM = 'StandRtgPerSum'  # Зависит от ЦА.
+    SALES_RTG_PER_SUM = 'SalesRtgPerSum'  # Не зависит от ЦА.
+    STAND_SALES_RTG_PER_SUM = 'StandSalesRtgPerSum'  # Не зависит от ЦА.
     SPOT_BY_BREAKS_RTG_PER_SUM = 'SpotByBreaksRtgPerSum'  # Зависит от ЦА.
     SPOT_BY_BREAKS_STAND_RTG_PER_SUM = 'SpotByBreaksStandRtgPerSum'  # Зависит от ЦА.
     SPOT_BY_BREAKS_SALES_RTG_PER_SUM = 'SpotByBreaksSalesRtgPerSum'  # Не зависит от ЦА.
@@ -28,20 +28,20 @@ class K7Statistic(Enum):
     CONSOLIDATED_COST_SUM_USD = 'ConsolidatedCostSumUSD'  # Не зависит от ЦА.
 
     # Средние статистики.
-    AV_REACH000 = 'AvReach000'
+    AV_REACH000 = 'AvReach000'  # Зависит от ЦА.
     SPOT_BY_BREAKS_AV_REACH000 = 'SpotByBreaksAvReach000'  # Зависит от ЦА.
-    AV_REACH_PER = 'AvReachPer'
+    AV_REACH_PER = 'AvReachPer'  # Зависит от ЦА.
     SPOT_BY_BREAKS_AV_REACH_PER = 'SpotByBreaksAvReachPer'  # Зависит от ЦА.
     SPOT_BY_BREAKS_OTS000_AVG = 'SpotByBreaksOTS000Avg'  # Зависит от ЦА.
     SPOT_BY_BREAKS_OTS_PER_AVG = 'SpotByBreaksOTSPerAvg'  # Зависит от ЦА.
     RTG000_AVG = 'Rtg000Avg'  # Зависит от ЦА.
-    SALES_RTG000_AVG = 'SalesRtg000Avg'
+    SALES_RTG000_AVG = 'SalesRtg000Avg'  # Не зависит от ЦА.
     SPOT_BY_BREAKS_RTG000_AVG = 'SpotByBreaksRtg000Avg'  # Зависит от ЦА.
     SPOT_BY_BREAKS_SALES_RTG000_AVG = 'SpotByBreaksSalesRtg000Avg'  # Не зависит от ЦА.
     RTG_PER_AVG = 'RtgPerAvg'  # Зависит от ЦА.
-    STAND_RTG_PER_AVG = 'StandRtgPerAvg'
-    SALES_RTG_PER_AVG = 'SalesRtgPerAvg'
-    STAND_SALES_RTG_PER_AVG = 'StandSalesRtgPerAvg'
+    STAND_RTG_PER_AVG = 'StandRtgPerAvg'  # Зависит от ЦА.
+    SALES_RTG_PER_AVG = 'SalesRtgPerAvg'  # Не зависит от ЦА.
+    STAND_SALES_RTG_PER_AVG = 'StandSalesRtgPerAvg'  # Не зависит от ЦА.
     SPOT_BY_BREAKS_RTG_PER_AVG = 'SpotByBreaksRtgPerAvg'  # Зависит от ЦА.
     SPOT_BY_BREAKS_STAND_RTG_PER_AVG = 'SpotByBreaksStandRtgPerAvg'  # Зависит от ЦА.
     SPOT_BY_BREAKS_SALES_RTG_PER_AVG = 'SpotByBreaksSalesRtgPerAvg'  # Не зависит от ЦА.
